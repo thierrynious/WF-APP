27050618881a70292eefc4698c0078c1bb1b0e54
Thierrynious <thierrynious@hotmail.fr>
2026-02-25 20:32:41 +0100

    Cloud ready for Render

--- src/test/java/com/finanzmanager/finanzapp/controller/TransactionControllerTest.java
+++ src/test/java/com/finanzmanager/finanzapp/controller/TransactionControllerTest.java
@@ -2,50 +2,48 @@ package com.finanzmanager.finanzapp.controller;
 
 import com.finanzmanager.finanzapp.model.Transaction;
 import com.finanzmanager.finanzapp.service.TransactionService;
-import org.junit.jupiter.api.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(TransactionController.class)
+@SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 class TransactionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private TransactionService transactionService;
 
     @Test
     void should_create_transaction_successfully() throws Exception {
-        Transaction tx = new Transaction(1L, "Miete", 1200.0, LocalDate.of(2023,10,1));
+
+        Transaction tx = new Transaction(1L, "Miete", 1200.0, LocalDate.of(2023, 10, 1));
         when(transactionService.save(any())).thenReturn(tx);
 
         mockMvc.perform(post("/api/transactions")
-                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
-                .content("""
-                        {"title":"Miete",
-                        "amount":1200.0,
-                        "date":"2023-10-01"
-                        }
-                        """)
-        ).andExpect(status().isCreated())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "title":"Miete",
+                                  "amount":1200.0,
+                                  "date":"2023-10-01"
+                                }
+                                """))
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.title").value("Miete"))
                 .andExpect(jsonPath("$.id").value(1));
-
     }
-
 }
--- src/test/java/com/finanzmanager/finanzapp/repository/TransactionRepositoryTest.java
+++ src/test/java/com/finanzmanager/finanzapp/repository/TransactionRepositoryTest.java
@@ -4,13 +4,12 @@ import com.finanzmanager.finanzapp.model.Transaction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class TransactionRepositoryTest {
@@ -26,7 +25,7 @@ class TransactionRepositoryTest {
         Transaction tx = new Transaction(
                 "Miete",
                 1000.0,
-                LocalDate.of(2025,1,1)
+                LocalDate.of(2025, 1, 1)
         );
 
         //when
